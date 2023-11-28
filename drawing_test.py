24560354205165cbd8258347ca2b0348e47d0e64
nickhib <root@LAPTOP-MMSI4IG9.localdomain>
2023-11-27 20:39:03 -0800

    making linter happy

--- drawing_test.py
+++ drawing_test.py
@@ -19,15 +19,13 @@ class DrawingApp:
         # Create a Save button
         save_button = tk.Button(master, text="Save", command=self.save_image)
         save_button.pack()
-
-
     # draw on the canvas during mouse movement
+
     def paint(self, events):
         x1, y1 = (events.x - 1), (events.y - 1)
         x2, y2 = (events.x + 1), (events.y + 1)
         # draws oval in black color or more accurate inking it in
         self.canvas.create_oval(x1, y1, x2, y2, fill="black", width=2)
-
 
     # now save image
     def save_image(self):
@@ -37,7 +35,6 @@ class DrawingApp:
         if path:
             # Save as png 
             self.canvas.postscript(file=path, colormode="color")
-
 
 # Entry point of the program
 if __name__ == "__main__":
